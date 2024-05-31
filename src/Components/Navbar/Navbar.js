96ecdf884707e51844c62b6f2a4010e0013c5682
Tanisha Bisht <agamyatani@gmail.com>
2024-05-31 18:32:36 -0400

    update navbar css

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Navbar, Nav} from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 import { Link } from 'react-scroll'
 import './navbar.scss'
 
@@ -8,14 +8,14 @@ import './navbar.scss'
 
 const NavbarComponent = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" variant="dark">
+    <Navbar className='navbar_container' collapseOnSelect expand="lg" variant="dark">
         <Navbar.Brand href="#">EESHAN DUTTA</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-        <nav className="ml-auto">
-          <Link className='secLink' to="employment" smooth={true}>Employement</Link>
-          <Link className='secLink' to="projects" smooth={true}>Projects</Link>
-        </nav>
+          <Nav className="ml-auto">
+            <Link className='secLink' to="employment" smooth={true}>Employement</Link>
+            <Link className='secLink' to="projects" smooth={true}>Projects</Link>
+          </Nav>
         </Navbar.Collapse>
     </Navbar>
   )
